5873feb3d55dbfbae36dde1de859a0a99106157b
venustheplanet <venustheplanet@users.noreply.github.com>
2021-08-17 21:14:37 +0300

    hw2

---
+++ src/main/java/homework/lab2/ClientConstants.java
@@ -0,0 +1,8 @@
+package homework.lab2;
+
+public interface ClientConstants {
+    int DEFAULT_DEBT = -100;
+    int DEFAULT_BONUS = -101;
+    String DEFAULT_NAME = "DEFAULT";
+    int DEFAULT_SALARY = -102;
+}
