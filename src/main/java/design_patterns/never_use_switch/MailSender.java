d1f43f2aaeb226c9c4726a88843a123de1ca74b5
venustheplanet <venustheplanet@users.noreply.github.com>
2021-08-20 13:42:34 +0300

    Applied Command pattern

--- src/main/java/design_patterns/never_use_switch/MailSender.java
+++ src/main/java/design_patterns/never_use_switch/MailSender.java
@@ -1,46 +1,37 @@
 package design_patterns.never_use_switch;
 
-import lombok.SneakyThrows;
-import org.reflections.Reflections;
-
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @author Evgeny Borisov
  */
 public class MailSender {
+    private static final Map<Integer, Command> actions = new HashMap<>();
+    static {
+        actions.put(1, new Command() {
+            @Override
+            public void send(MailInfo mailInfo) {
+                //50 lines of code which send welcome mail
+                System.out.println("Welcome mail was sent "+mailInfo.getContext());
+            }
+        });
 
+        actions.put(2, new Command() {
+            @Override
+            public void send(MailInfo mailInfo) {
+                //50 lines of code which send welcome mail
+                System.out.println("Happy Birthday mail was sent "+mailInfo.getContext());
+            }
+        });
 
-    private Map<Integer,MailGenerator> mailGeneratorMap = new HashMap<>();
-
-
-    @SneakyThrows
-    public MailSender() {
-        Reflections scanner = new Reflections("design_patterns.never_use_switch");
-        Set<Class<? extends MailGenerator>> classes = scanner.getSubTypesOf(MailGenerator.class);
-        for (Class<? extends MailGenerator> generatorClass : classes) {
-            MailGenerator mailGenerator = generatorClass.getDeclaredConstructor().newInstance();
-            mailGeneratorMap.put(mailGenerator.myCode(), mailGenerator);
-        }
     }
 
     public void send(MailInfo mailInfo) {
-        int mailType = mailInfo.getMailType();
-        MailGenerator mailGenerator = mailGeneratorMap.get(mailType);
-        if (mailGenerator == null) {
-            throw new UnsupportedOperationException(mailType + " not supported yet");
+        Command command = actions.get(mailInfo.getMailType());
+        if (command == null) {
+            throw new IllegalArgumentException("Invalid mail type=" + mailInfo.getMailType());
         }
-        String mail = mailGenerator.generateMail(mailInfo);
-
-
-        send(mail);
-
-
-    }
-
-    private void send(String mail) {
-        System.out.println("sending... "+mail);
+        command.send(mailInfo);
     }
 }
