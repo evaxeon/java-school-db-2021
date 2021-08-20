74309bff0181a77a04ddeb4d7dd95d30a17a747c
venustheplanet <venustheplanet@users.noreply.github.com>
2021-08-20 13:19:50 +0300

    merging

--- src/main/java/homework/lab2/ClientConstants.java
+++
@@ -1,8 +0,0 @@
-package homework.lab2;
-
-public interface ClientConstants {
-    int DEFAULT_DEBT = -100;
-    int DEFAULT_BONUS = -101;
-    String DEFAULT_NAME = "DEFAULT";
-    int DEFAULT_SALARY = -102;
-}
