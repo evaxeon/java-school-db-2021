76e7ad9817d5e20df0a82bb6968a9669f0574dae
venustheplanet <venustheplanet@users.noreply.github.com>
2021-08-20 13:42:34 +0300

    Applied Command pattern

---
+++ src/main/java/design_patterns/never_use_switch/Command.java
@@ -0,0 +1,5 @@
+package design_patterns.never_use_switch;
+
+public interface Command {
+    void send(MailInfo mailinfo);
+}
--- src/main/java/design_patterns/never_use_switch/MailSender.java
+++ src/main/java/design_patterns/never_use_switch/MailSender.java
@@ -1,19 +1,37 @@
 package design_patterns.never_use_switch;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Evgeny Borisov
  */
 public class MailSender {
+    private static final Map<Integer, Command> actions = new HashMap<>();
+    static {
+        actions.put(1, new Command() {
+            @Override
+            public void send(MailInfo mailInfo) {
+                //50 lines of code which send welcome mail
+                System.out.println("Welcome mail was sent "+mailInfo.getContext());
+            }
+        });
+
+        actions.put(2, new Command() {
+            @Override
+            public void send(MailInfo mailInfo) {
+                //50 lines of code which send welcome mail
+                System.out.println("Happy Birthday mail was sent "+mailInfo.getContext());
+            }
+        });
+
+    }
 
     public void send(MailInfo mailInfo) {
-        //todo refactor this to beautiful code
-        if (mailInfo.getMailType() == 1) {
-            //50 lines of code which send welcome mail
-            System.out.println("Welcome mail was sent "+mailInfo.getContext());
-        } else if (mailInfo.getMailType() == 2) {
-            //50 lines of code which send welcome mail
-            System.out.println("Happy Birthdate mail was sent "+mailInfo.getContext());
+        Command command = actions.get(mailInfo.getMailType());
+        if (command == null) {
+            throw new IllegalArgumentException("Invalid mail type=" + mailInfo.getMailType());
         }
-
+        command.send(mailInfo);
     }
 }
