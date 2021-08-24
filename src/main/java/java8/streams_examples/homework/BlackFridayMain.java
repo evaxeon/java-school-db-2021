fbe5afca6870269313a78abaa9d10ba62e9398f6
venustheplanet <venustheplanet@users.noreply.github.com>
2021-08-24 20:32:37 +0300

    BlackFridayService added

---
+++ src/main/java/java8/streams_examples/homework/BlackFridayMain.java
@@ -0,0 +1,7 @@
+package java8.streams_examples.homework;
+
+public class BlackFridayMain {
+    public static void main(String[] args) {
+        BlackFridayService.printBlackFridayPerYearSorted(1995, 2036);
+    }
+}
